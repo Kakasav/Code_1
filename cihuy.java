834d87b59de63f73bf142a7da2ba868a69307a1b
Kakasav <felishaaureliajessalyn@gmail.com>
2024-09-21 20:54:08 +0800

    Add files via upload

---
+++ cihuy.java
@@ -0,0 +1,11 @@
+public class cihuy {
+
+    public static void main(String[] args) {
+        String nama;
+        System.out.print("nama: Felisha " );
+        System.out.println();
+        int umur;
+        System.out.print("umur: 18 ");
+  
+    }
+}
